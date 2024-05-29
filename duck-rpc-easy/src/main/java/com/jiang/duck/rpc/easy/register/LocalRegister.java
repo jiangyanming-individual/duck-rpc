84366363a6cddbea00654312720ef6a192f078d3
jiangyanming <1635156042@qq.com>
2024-05-29 22:36:37 +0800

    init commit

--- duck-rpc-easy/src/main/java/com/jiang/duck/rpc/easy/register/LocalRegister.java
+++ duck-rpc-easy/src/main/java/com/jiang/duck/rpc/easy/register/LocalRegister.java
@@ -1,2 +1,41 @@
-package com.jiang.duck.rpc.easy.register;public class LocalRegister {
+package com.jiang.duck.rpc.easy.register;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 本地服务注册器:
+ */
+public class LocalRegister {
+    /**
+     * 存储注册服务
+     */
+    public static final Map<String,Class<?>> map=new ConcurrentHashMap<String,Class<?>>();
+
+    /**
+     * 注册服务
+     * @param serviceName
+     * @param implClass  Class类
+     */
+    public static void register(String serviceName,Class<?> implClass){
+        map.put(serviceName,implClass);
+    }
+
+    /**
+     * 获取服务
+     * @param serviceName
+     * @return
+     */
+    public static Class<?> get(String serviceName){
+        return map.get(serviceName);
+    }
+
+    /**
+     * 移除服务
+     * @param serviceName
+     */
+    public static void  remove(String serviceName){
+        map.remove(serviceName);
+    }
+
 }
--- example-provider/src/main/java/com/jiang/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/jiang/example/provider/EasyProviderExample.java
@@ -1,9 +1,15 @@
 package com.jiang.example.provider;
 
+import com.jiang.duck.rpc.easy.register.LocalRegister;
 import com.jiang.duck.rpc.easy.server.VertxHttpServer;
+import com.jiang.example.common.service.UserService;
 
 public class EasyProviderExample {
     public static void main(String[] args) {
+        //注册服务
+        LocalRegister.register(UserService.class.getName(),UserServiceImpl.class);
+
+        //启动web服务器:
         VertxHttpServer vertxHttpServer = new VertxHttpServer();
         vertxHttpServer.doStart(8020);
     }
